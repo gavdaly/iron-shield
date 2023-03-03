463b9be6bdeae08cd8b849c3895891b72c26f129
Gavin Daly <gavdaly@protonmail.com>
2023-03-03 13:15:48 -0500

    show military and standard time

--- static/script.js
+++ static/script.js
@@ -6,11 +6,19 @@ let clockInterval;
 function setClock() {
     let clock = document.getElementById("clock");
     if (clock) {
-        clockInterval = setInterval(()=> {
+        let data = clock.dataset.format
+        clockInterval = setInterval(() => {
             let time = new Date();
-            clock.innerText = `${time.getHours()}:${time.getMinutes()}`
+            if (data == "military") {
+                clock.innerText = `${time.getHours()}:${time.getMinutes()}`
+            }
+            else {
+                let hours = time.getHours()
+                let apm = (hours % 12 == 0) ? 'am' : 'pm'
+                clock.innerText = `${Math.floor(hours / 12)}:${time.getMinutes()} ${apm}`
+            }
         }, 100)
     }
 }
 
-document.onclose(() => {clearInterval(clockInterval)})
+document.onclose(() => { clearInterval(clockInterval) })
