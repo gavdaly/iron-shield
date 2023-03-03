2de6645d768661b8f751925af6c702aa23070a64
Gavin Daly <gavdaly@protonmail.com>
2023-03-03 05:21:02 -0500

    serve static files

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,9 @@
-use crate::index::generate_index;
-use axum::{routing::get, Router};
 mod config;
 mod index;
+mod server;
 mod sites;
 
 #[tokio::main]
-async fn main() -> Result<(), ()> {
-    let app = Router::new().route("/", get(generate_index));
-
-    let address = &"0.0.0.0:3000".parse().expect("Error parsing address");
-
-    axum::Server::bind(address)
-        .serve(app.into_make_service())
-        .await
-        .expect("Run Server");
-
-    println!("Site launched on: http://0.0.0.0:3000");
-
-    Ok(())
+async fn main() {
+    server::run().await;
 }
---
+++ src/server.rs
@@ -0,0 +1,18 @@
+use crate::index::generate_index;
+use axum::{routing::get, Router};
+use tower_http::services::ServeDir;
+
+pub async fn run() -> () {
+    let app = Router::new()
+        .route("/", get(generate_index))
+        .nest_service("/", ServeDir::new("static"));
+
+    let address = &"0.0.0.0:3000".parse().expect("Error parsing address");
+
+    axum::Server::bind(address)
+        .serve(app.into_make_service())
+        .await
+        .expect("Run Server");
+
+    println!("Site launched on: http://0.0.0.0:3000");
+}
